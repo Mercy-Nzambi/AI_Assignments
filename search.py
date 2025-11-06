0c49eb50556bb9691958ee5f406d0438b170227e
Mercy-Nzambi <mercymbuthi5@gmail.com>
2025-11-07 02:51:59 +0300

    Updated assignment 1 files

--- search.py
+++ search.py
@@ -15,7 +15,7 @@ import util
 
 class SearchProblem:
   """
-  This class outlines the structure of a search problem, but doesn't implement
+  This class. outlines the structure of a search problem, but doesn't implement
   any of the methods (in object-oriented terminology: an abstract class).
   
   You do not need to. change anything in this class, ever.
