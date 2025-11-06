3f4791bb147e948853431547f615bdba1ffa05e4
Mercy-Nzambi <mercymbuthi5@gmail.com>
2025-11-07 02:42:40 +0300

    Modified search.py file

--- search.py
+++ search.py
@@ -18,7 +18,7 @@ class SearchProblem:
   This class outlines the structure of a search problem, but doesn't implement
   any of the methods (in object-oriented terminology: an abstract class).
   
-  You do not need to change anything in this class, ever.
+  You do not need to. change anything in this class, ever.
   """
   
   def getStartState(self):
